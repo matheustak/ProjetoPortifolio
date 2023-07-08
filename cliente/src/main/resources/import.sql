32b6f910b2fc8129efd2dbd4b8b274b3d4723be0
matheus <129976192+matheustak@users.noreply.github.com>
2023-07-07 20:00:13 -0400

    Classe,seed,projeto
    projeto spring boot jpa gerando tabela e dando carga no seed de dados

---
+++ cliente/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Fernanda Didi','05477955638','600.0','3','2009-07-25');
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Isa Luz','05477955638','650.0','4','2008-01-20');
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Fernanda Luz','05477955841','555.0','6','2010-03-20'); 
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Jaque Didi','05477955444','100.0','3','2004-01-30');
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Bru Luz','05477955555','220.0','5','2020-02-25');
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Cau Luz','05477955841','999.0','2','2030-04-11'); 
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Jaqueline Luz','05477955841','555.0','6','2010-03-20'); 
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Jesabel Didi','05477955444','100.0','3','2014-01-30');
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Clara Luz','05477955555','220.0','5','2020-02-25');
+INSERT INTO tb_client(name, cpf ,income ,children ,birth_date) VALUES ('Kakau Luz','05477955841','999.0','2','2031-06-21'); 
+
+
