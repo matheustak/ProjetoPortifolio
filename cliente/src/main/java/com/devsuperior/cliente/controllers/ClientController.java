54cf0bea9affd24065ad9c9231dd46a3018667dc
matheus <129976192+matheustak@users.noreply.github.com>
2023-07-09 14:58:14 -0400

    Delete
    Apagando cliente com deleteMapping por id

--- cliente/src/main/java/com/devsuperior/cliente/controllers/ClientController.java
+++ cliente/src/main/java/com/devsuperior/cliente/controllers/ClientController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,15 @@ public class ClientController {
 		return ResponseEntity.ok(dto);
 		
 	}
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity <Void> update(@PathVariable long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+			
 			
 
 }
